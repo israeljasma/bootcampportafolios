80221e22347fd58b4b51479cc48b0e7242ed063b
Israel Jasma <israel.jasma@gmail.com>
2023-05-21 04:27:17 -0400

    Feat: diseño login, servlet login

---
+++ src/main/java/cl/israeljasma/apiservlet/webapp/controllers/Login.java
@@ -0,0 +1,17 @@
+package cl.israeljasma.apiservlet.webapp.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/login.html").forward(req, resp);
+    }
+}
